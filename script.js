698e6ec4e78a2c0932e6bb896b85bae86c9eb276
kartikeya <kartikeyasingh.ks43@gmail.com>
2022-12-20 19:47:16 +0530

    added all files

--- script.js
+++ script.js
@@ -1,113 +1,155 @@
-let o = console.log;
-
 function nowcreatemytask() {
 
   let t = document.getElementById("tasktitle").value;
   let d = document.getElementById("taskdesc").value;
-  if (localStorage.getItem('items') == null) {
-    itemsjson = []
-    itemsjson.push([t, d])
-    // o(itemsjson) // ARAAY
-    localStorage.setItem('items', JSON.stringify(itemsjson)); // STRING
+  let payloaddata = {
+    id: '',
+    todotitle: t,
+    tododescription: d
 
   }
-  else {
-    let getfromitems = localStorage.getItem('items')
-    o(getfromitems)
-    itemsjson = JSON.parse(getfromitems); // 
-    itemsjson.push([t, d]);
-    localStorage.setItem('items', JSON.stringify(itemsjson));
-
+  let options = {
+    method: 'POST',
+    body: JSON.stringify(payloaddata),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   }
-  up();
+
+  postapicall(options);
+  getapicall();
 }
-
-function up() {
-  let str = "";
-
-  if (localStorage.getItem('items') == null) {
-    itemsjson = []
-    // localStorage.setItem('items', JSON.stringify(itemsjson));
-
-  }
-  else {
-    let getfromitems = localStorage.getItem('items')
-    itemsjson = JSON.parse(getfromitems);
-    localStorage.setItem('items', JSON.stringify(itemsjson));
-
-  }
-  for (let index in itemsjson) {
-    str +=
-      `
-                <tbody id="todobody">
-        <tr>
-          <th scope="row">${index}</th>
-          <td>${itemsjson[index][0]}</td>
-          <td>${itemsjson[index][1]}</td>
-          <td>
-          <button
-          class="btn btn-success"
-          id="savechanges"
-          onclick="saving(${index})"
-        >
-          save
-        </button>
-        </td>
-          <td><button class="btn btn-secondary" onclick="updatetodo(${index})">
-          Update
-        </button></td>
-       
-          <td>
-            <button class="btn btn-danger" onclick="deletetodo(${index})">
-              Delete
-            </button>
-          </td>
-        </tr>
-      </tbody>`
-  }
-  document.getElementById("todobody").innerHTML = str;
-}
-up();
 
 function deletetodo(indexxx) {
-  let getfromitems = localStorage.getItem('items')
-  itemsjson = JSON.parse(getfromitems);
-  o(itemsjson)
-  itemsjson.splice(indexxx, 1)
-  localStorage.setItem('items', JSON.stringify(itemsjson));
-  up();
+  console.log(indexxx);
 
+
+  let deleteurl = "http://localhost:8080/deletetodo/" + indexxx;
+  console.log(deleteurl);
+  deleteapicall(deleteurl);
+  getapicall();
 }
-function updatetodo(indec) {
-  o("this is indec" + indec)
 
-  let getfromitems = localStorage.getItem('items')
-  itemsjson = JSON.parse(getfromitems);
-  o(itemsjson)
+
+function updatetodo(indec, index) {
+  console.log("this is indec" + indec)
+  let updateurl = "http://localhost:8080/updatetodo/" + indec
+  fetch(geturl)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json[index])
+      document.getElementById("tasktitle").value = json[index].todotitle;
+
+      document.getElementById("taskdesc").value = json[index].tododescription;
+
+    });
   document.getElementById("bone").style.display = 'none';
   document.getElementById("savechanges").style.display = 'block';
-  document.getElementById("tasktitle").value = itemsjson[indec][0];
 
-  document.getElementById("taskdesc").value = itemsjson[indec][1];
 }
 
-function saving(lo) {
-  o("this is vvv" + lo)
-  let getfromitems = localStorage.getItem('items')
-  itemsjson = JSON.parse(getfromitems);
+function saving(lo, index) {
+  console.log("this is vvv" + lo)
+  let newupdateapiurl = "http://localhost:8080/updatetodo/" + lo;
   let ft = document.getElementById("tasktitle").value;
   let fd = document.getElementById("taskdesc").value;
-  o(ft, fd)
-
-  deletetodo(lo)
-
-  itemsjson.push([ft, fd])
-
-  localStorage.setItem('items', JSON.stringify(itemsjson));
-  up();
-
+  console.log("new" + ft, fd)
+  let payloadfornewdata = {
+    id: lo,
+    todotitle: ft,
+    tododescription: fd
+  }
   // document.getElementById("savechanges").style.display = 'none';
   document.getElementById("bone").style.display = 'block';
+  fetch(newupdateapiurl, {
+    method: 'PUT',
+    body: JSON.stringify(payloadfornewdata),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      getapicall();
+      document.getElementById("tasktitle").value = "";
+
+      document.getElementById("taskdesc").value = "";
+    });
+}
+const updateapicall = (updateurl) => {
 
 }
+let geturl = "http://localhost:8080/getalltodo";
+let posturl = " http://localhost:8080/addtodo";
+let updateurl = "";
+
+const getapicall = () => {
+  fetch(geturl)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("running");
+      let str = "";
+
+      let geturl = "http://localhost:8080/getalltodo";
+      fetch(geturl)
+        .then((response) => response.json())
+        .then((json) => {
+
+          for (let index in json) {
+            console.log("inside");
+            str +=
+              `
+                        <tbody id="todobody">
+                <tr>
+                  <th scope="row">${+index + 1}</th>
+                  <td>${json[index].todotitle}</td>
+                  <td>${json[index].tododescription}</td>
+                  <td>
+                  <button
+                  class="btn btn-success"
+                  id="savechanges"
+                  onclick="saving(${json[index].id},${index})"
+                >
+                  save
+                </button>
+                </td>
+                  <td><button class="btn btn-secondary" onclick="updatetodo(${json[index].id},${index})">
+                  Update
+                </button></td>
+    
+                  <td>
+                    <button class="btn btn-danger" onclick="deletetodo(${json[index].id})">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>`
+          }
+          document.getElementById("todobody").innerHTML = str;
+
+        });
+
+    });
+}
+getapicall();
+
+const postapicall = (options) => {
+  fetch(posturl, options)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      document.getElementById("tasktitle").value = "";
+
+      document.getElementById("taskdesc").value = "";
+    });
+}
+
+const deleteapicall = (deleteurl) => {
+  console.log("in delte api call");
+
+  fetch(deleteurl, {
+    method: 'DELETE',
+  })
+};
 
